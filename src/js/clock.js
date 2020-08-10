edec835e148de6e9d7e398386bd81791303796cc
zpskek <wjdghdwns@gmail.com>
2020-08-10 09:49:11 +0900

    Make jsClock

---
+++ src/js/clock.js
@@ -0,0 +1,19 @@
+const clock = document.getElementById('jsClock');
+
+const handleTime = () => {
+  const time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+
+  if (hours < 10) hours = `0${hours}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (seconds < 10) seconds = `0${seconds}`;
+  clock.innerHTML = `${hours}:${minutes}:${seconds}`;
+};
+
+const init = () => {
+  setInterval(handleTime, 1000);
+};
+
+init();
