debef91c92098b45bed13a789cba4d985da5f457
zpskek <wjdghdwns@gmail.com>
2020-08-16 10:48:54 +0900

    Fix api error

--- src/js/weather.js
+++ src/js/weather.js
@@ -7,24 +7,6 @@ const saveItem = (coordsObj) => {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 };
 
-const handleGeoSuccess = (position) => {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  const coordsObj = {
-    latitude,
-    longitude,
-  };
-  saveItem(latitude, longitude);
-};
-
-const handleGeoError = () => {
-  console.log('Cant access geo loacation');
-};
-
-const askCoords = () => {
-  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
-};
-
 const setCoords = (lat, lon) => {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
@@ -46,10 +28,29 @@ const setCoords = (lat, lon) => {
     });
 };
 
+const handleGeoSuccess = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveItem(coordsObj);
+  setCoords(latitude,longitude);
+};
+
+const handleGeoError = () => {
+  console.log('Cant access geo loacation');
+};
+
+const askCoords = () => {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+};
+
 function init() {
   const loadedCoords = localStorage.getItem(COORDS);
 
-  if (loadedCoords === null) {
+  if (loadedCoords === null || ) {
     askCoords();
   } else {
     const parsedCoords = JSON.parse(loadedCoords);
