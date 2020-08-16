26e6e4ee3765a8fa8155fa202bca7f04b273beae
zpskek <wjdghdwns@gmail.com>
2020-08-16 10:17:49 +0900

    Change file directory on bg.js

--- src/js/bg.js
+++ src/js/bg.js
@@ -9,7 +9,7 @@ const genRandom = () => {
 
 const paintImage = (imgNumber) => {
   body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
-  url('../images/com/bg${imgNumber + 1}.jpg')`;
+  url('./images/com/bg${imgNumber + 1}.jpg')`;
   body.classList.add('bgImage');
 };
 
